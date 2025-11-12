8e72685b3e4a82910bd84c0f024afb118034185f
LJW <ljw867424844@gmail.com>
2025-11-12 16:03:13 +0800

    '复习第一章'

---
+++ exercise1_DataTypes&Variables.py
@@ -0,0 +1,43 @@
+"""
+>>> print('I\'m \"ok\"')
+I'm "ok"
+>>> print('I\'m \t fine')
+I'm      fine
+>>> print('I\'m learning\nPython')
+I'm learning
+Python
+>>> print('\\\n\\')
+\
+\
+>>> print(r'\\\n\\')
+\\\n\\
+>>> print('''word1
+... word2
+... word3''')
+word1
+word2
+word3
+>>> 5 > 4
+True
+>>> 4 > 5
+False
+>>> True and False
+False
+>>> True or False
+True
+>>> not False
+True
+>>> a = 'ABC'
+>>> b = a
+>>> a = 'DEF'
+>>> print(b)
+ABC
+>>> 11 / 3
+3.6666666666666665
+>>> 9 / 3
+3.0
+>>> 11 // 3
+3
+>>> 11 % 3
+2
+"""
---
+++ exercise3_List&Tuple.py
@@ -0,0 +1,13 @@
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Bob']
+]
+
+# 取Apple:
+assert L[0][0] == 'Apple'
+# 取Python:
+assert L[1][1] == 'Python'
+# 取Bob:
+assert L[-1][-1] == 'Bob'
+print('OK')
